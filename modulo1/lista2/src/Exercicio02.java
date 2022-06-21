22ee85fad3959b7a062b0f68b3c5a73138ff00c8
Alyson Campos <alyson.siqueiracampos@gmail.com>
2022-06-21 12:58:40 -0300

    Uptade Exercicio02 lista2

--- modulo1/lista2/src/Exercicio02.java
+++ modulo1/lista2/src/Exercicio02.java
@@ -3,29 +3,31 @@ import java.util.Scanner;
 
 public class Exercicio02 {
     public static void main(String[] args){
-        int min = 1, max = 10, numEscolhido;
+
+        Integer numEscolhido, numSorteado;
+        int min = 1, max = 10;
 
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int numSorteado = random.nextInt(max + min) + min;
+        numSorteado = random.nextInt(max + min) + min;
         System.out.println("Seja bem vindo!");
-        System.out.print("Escolha um número de 1 a 10: ");
-        numEscolhido = sc.nextInt();
 
-        while(numEscolhido != numSorteado){
-            if(numEscolhido < numSorteado) {
-                System.out.println("O número a ser encontrado é menor do que o que você digitou!");
+        while(true){
+            System.out.print("Escolha um número de 1 a 10: ");
+            numEscolhido = sc.nextInt();
+
+            if(numEscolhido.equals(numSorteado)) {
+                System.out.printf("Número sorteado foi %d. Parabéns, você acertou!%n",numSorteado);
+                break;
+            }
+            else if(numEscolhido > numSorteado) {
+                System.out.printf("O número a ser encontrado é MENOR que %d%n",numEscolhido);
             }
             else {
-                System.out.println("O número a ser encontrado é maior do que o que você digitou!");
+                System.out.printf("O número a ser encontrado é MAIOR que %d%n",numEscolhido);
             }
-            System.out.printf("Escolha um número de 1 a 10: ");
-            numEscolhido = sc.nextInt();
         }
-
-        System.out.printf("Número sorteado foi %d. Parabéns, você acertou!", numSorteado);
-
         sc.close();
     }
 }
