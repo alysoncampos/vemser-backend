245cb22e4582939256cb13dc2f3a77affeb10ca7
Alyson Campos <alyson.siqueiracampos@gmail.com>
2022-06-22 10:49:36 -0300

    Uptaded class ContaCorrente

--- modulo1/conta-corrente/src/ContaCorrente.java
+++ modulo1/conta-corrente/src/ContaCorrente.java
@@ -16,37 +16,19 @@ public class ContaCorrente {
     }
 
     public boolean sacar(double valor){
-        if(valor > retornarSaldoComChequeEspecial()){
-//            System.out.println("Você não possui fundos suficientes para sacar!");
-            return false;
-        }
-        if(valor < 0.0){
-//            System.out.println("Valor de saque deve ser maior do que zero!");
-            return false;
-        }
-        if(saldo >= valor){
+        if(valor <= retornarSaldoComChequeEspecial() && valor > 0){
             saldo -= valor;
-//            System.out.printf("Saque de R$ %.2f realizado com sucesso!%n",valor);
-//            System.out.printf("Seu novo saldo é: R$ %.2f %n",saldo);
-//            System.out.printf("Limite de Cheque Especial: R$ %.2f",chequeEspecial);
-        } else if (retornarSaldoComChequeEspecial() >= valor){
-            saldo -= valor;
-//            System.out.printf("Saque de R$ %.2f realizado com sucesso!%n",valor);
-//            System.out.printf("Seu novo saldo é: R$ %.2f %n",saldo);
-//            System.out.printf("Limite de Cheque Especial: R$ %.2f",chequeEspecial);
+            return true;
         }
-        return true;
+        return false;
     }
 
     public boolean depositar(double valor){
-        if(valor <= 0.0) {
-//            System.out.println("Deposite um valor válido");
-            return false;
+        if(valor > 0.0) {
+            saldo += valor;
+            return true;
         }
-        saldo += valor;
-//        System.out.printf("Você depositou: R$ %.2f %n" +
-//                          "Seu novo saldo: R$ %.2f %n", valor, saldo);
-        return true;
+        return false;
     }
 
     public double retornarSaldoComChequeEspecial(){
@@ -54,27 +36,11 @@ public class ContaCorrente {
     }
 
     public boolean transferir(ContaCorrente cc, double valor){
-        if(valor > retornarSaldoComChequeEspecial()){
-//            System.out.println("Você não possui fundos suficientes para transferir!");
-            return false;
-        }
-        if(valor < 0.0){
-//            System.out.println("Valor deve ser maior do que zero!");
-            return false;
-        }
-        if (this.saldo >= valor){
-            cc.saldo += valor;
+        if(valor <= retornarSaldoComChequeEspecial() && valor > 0){
             this.saldo -= valor;
-//            System.out.printf("Você transferiu R$ %.2f para %s %n", valor, cc.cliente.nome);
-//            System.out.printf("Novo saldo: R$ %.2f %n", this.saldo);
-//            System.out.printf("Limite de Cheque Especial: R$ %.2f", chequeEspecial);
-        } else if (retornarSaldoComChequeEspecial() >= valor){
             cc.saldo += valor;
-            this.saldo -= valor;
-//            System.out.printf("Você transferiu R$ %.2f para %s %n", valor, cc.cliente.nome);
-//            System.out.printf("Novo saldo: R$ %.2f %n", saldo);
-//            System.out.printf("Limite de Cheque Especial: R$ %.2f", chequeEspecial);
+            return true;
         }
-        return true;
+        return false;
     }
 }
