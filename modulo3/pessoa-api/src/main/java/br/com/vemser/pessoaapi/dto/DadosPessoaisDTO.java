5f43ebd47dffb1a8354fe21e49a93ab2b01de50c
Alyson Campos <alyson.siqueiracampos@gmail.com>
2022-07-13 18:26:44 -0300

    Implementação Feign Client

---
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/dto/DadosPessoaisDTO.java
@@ -0,0 +1,22 @@
+package br.com.vemser.pessoaapi.dto;
+
+import br.com.vemser.pessoaapi.entity.Sexo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class DadosPessoaisDTO {
+
+    private String cnh;
+    private String cpf;
+    private String nome;
+    private String nomeMae;
+    private String nomePai;
+    private String rg;
+    private Sexo sexo;
+    private String tituloEleitor;
+
+}
